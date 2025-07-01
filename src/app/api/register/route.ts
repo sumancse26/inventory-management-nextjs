7618388f8b814160ca7cdf0cba478f8b523acef8
Suman Sarkar <suman.iu32@gmail.com>
2025-07-01 18:38:01 +0600

    fix: new env added

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,78 +1,67 @@
-import prisma from "@/config/prisma";
-import { encryptPassword } from "@/utils";
-import { NextRequest, NextResponse } from "next/server";
+import prisma from '@/config/prisma';
+import { encryptPassword } from '@/utils';
+import { NextRequest, NextResponse } from 'next/server';
 
 interface UserBody {
-  first_name: string;
-  last_name: string;
-  email: string;
-  mobile: string;
-  password: string;
-  otp: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    mobile: string;
+    password: string;
+    otp: string;
 }
 
 export const POST = async (req: NextRequest): Promise<NextResponse> => {
-  try {
-    const body: UserBody = await req.json();
-    if (
-      !body.first_name ||
-      !body.last_name ||
-      !body.mobile ||
-      !body.email ||
-      !body.password
-    ) {
-      return NextResponse.json(
-        { error: "Missing required fields" },
-        { status: 400 }
-      );
+    try {
+        const body: UserBody = await req.json();
+        if (!body.first_name || !body.last_name || !body.mobile || !body.email || !body.password) {
+            return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+        }
+        const existingUser = await prisma.users.findUnique({
+            where: {
+                email: body.email
+            }
+        });
+        if (existingUser?.email) {
+            return NextResponse.json({ message: 'User already exists', success: false }, { status: 400 });
+        }
+
+        const hashedPassword = await encryptPassword(body.password);
+
+        const user = await prisma.users.create({
+            data: {
+                first_name: body.first_name,
+                last_name: body.last_name,
+                email: body.email,
+                mobile: body.mobile,
+                password: hashedPassword,
+                otp: '0'
+            }
+        });
+
+        return NextResponse.json(
+            {
+                message: 'User created successfully',
+                success: true,
+                data: {
+                    id: user.id,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    email: user.email,
+                    mobile: user.mobile
+                }
+            },
+            { status: 200 }
+        );
+    } catch (err: unknown) {
+        console.log(err);
+        return NextResponse.json(
+            {
+                error: 'Internal Server Error'
+            },
+            { status: 500 }
+        );
     }
-    const existingUser = await prisma.users.findUnique({
-      where: {
-        email: body.email,
-      },
-    });
-    if (existingUser?.email) {
-      return NextResponse.json(
-        { message: "User already exists", success: false },
-        { status: 400 }
-      );
-    }
-
-    const hashedPassword = await encryptPassword(body.password);
-
-    const user = await prisma.users.create({
-      data: {
-        first_name: body.first_name,
-        last_name: body.last_name,
-        email: body.email,
-        mobile: body.mobile,
-        password: hashedPassword,
-        otp: "0",
-      },
-    });
-
-    return NextResponse.json(
-      {
-        message: "User created successfully",
-        success: true,
-        data: {
-          id: user.id,
-          first_name: user.first_name,
-          last_name: user.last_name,
-          email: user.email,
-          mobile: user.mobile,
-        },
-      },
-      { status: 200 }
-    );
-  } catch (err: unknown) {
-    return NextResponse.json(
-      {
-        error: err instanceof Error ? err.message : "Internal Server Error",
-      },
-      { status: 500 }
-    );
-  }
 };
 
 // export const GET = async (req: NextRequest): Promise<NextResponse> => {};

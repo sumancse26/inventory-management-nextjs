472c56371d7f3ecf268f5fe22ede5bd770a3f868
Suman Sarkar <suman.iu32@gmail.com>
2025-07-01 16:48:33 +0600

    Fix useSearchParams with Suspense boundary

--- src/app/submit-otp/page.js
+++ src/app/submit-otp/page.js
@@ -2,9 +2,14 @@
 
 export const dynamic = 'force-dynamic';
 import SubmitOtp from '@components/submitOtp';
+import { Suspense } from 'react';
 
 const SubmitOtpPage = () => {
-    return <SubmitOtp />;
+    return (
+        <Suspense fallback={<p>......</p>}>
+            <SubmitOtp />
+        </Suspense>
+    );
 };
 
 export default SubmitOtpPage;

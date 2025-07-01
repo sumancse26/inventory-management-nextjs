c13489b47d4be3ddc51d7e04f8d9ec81538caca4
Suman Sarkar <suman.iu32@gmail.com>
2025-07-01 16:24:19 +0600

    feat: make dynamic rendering

--- src/components/submitOtp.jsx
+++ src/components/submitOtp.jsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { submitOtpAction } from '@/app/actions/authAction';
 import { useAlert } from '@/context/AlertContext';
 import { useApiLoader } from '@/lib/useApiLoader';
